d12051ef5c84a8e5a57dd2bf1f45c3323ac0f7f6
Rémi <remi.momprive@gmail.com>
2017-03-02 15:35:38 +0100

    Fix middleware

--- app/Http/Controllers/AdherentController.php
+++ app/Http/Controllers/AdherentController.php
@@ -10,6 +10,12 @@ use Illuminate\Http\Request;
 
 class AdherentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
