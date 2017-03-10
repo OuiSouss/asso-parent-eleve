fa0ac2cc76c4d82f0cc6c4f65d60789105c9776f
Antoine Poussard <antoine.poussard@etu.u-bordeaux.fr>
2017-03-10 11:01:12 +0100

    OrderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Adherent;
+use App\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Order::all();
+        $adherent = Adherent::all();
+        return view('admin.orders.index', ['page_title' => 'Liste des commandes', 'orders' => $orders, 'adherent' => $adherent]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $order = new Order();
+        return view('admin.orders.form', ['page_title' => 'Nouvelle commande', 'order' => $order]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $order = Order::create($request->all());
+        return redirect()->route('orders.show', $order);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $order = Order::find($id);
+        return view('admin.orders.show', ['page_title' => 'Informations sur la commande', 'order' => $order]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $order = Order::find($id);
+        return view('admin.orders.form', ['page_title' => 'Nouvelle commande', 'order' => $order]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $order = Order::find($id);
+        $order->update($request->all());
+        return redirect()->route('adherents.show', $order);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $order = Order::find($id);
+
+        if (!is_null($order)) {
+            $order->delete();
+
+            return response()->json([
+                'status' => 'success',
+            ]);
+        }
+    }
+}
